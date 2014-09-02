b68077dc8fb8ea43a9065230483420ea103e9aaa
Nathan Vander Wilt <natevw@yahoo.com>
2014-09-02 14:46:21 -0700

    Adds package metadata via setup.py
    I based this on an example at http://guide.python-distribute.org/creation.html with your GitHub project/user info. I went out on a limb and said it requires Django 1.4 based on your claim that it works on 1.5 and likely earlier.
    
    This file seems to be all that's necessary to `pip install` via a repo URL and should make submission to PyPI (#1) much easier for even simpler re-use of this code.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='django-sameas',
+    version='0.1.0',
+    author='Yordan Miladinov',
+    author_email='jordanMiladinov@gmail.com',
+    packages=['sameas'],
+    url='https://github.com/ydm/django-sameas',
+    license='LICENSE',
+    description='Django application that provides you with an easy-to-use template tag that replicates a block.',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "Django >= 1.4"
+    ],
+)
